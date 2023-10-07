61b9ca316a95feceae4891b7f96f9712d23e51a1
Marta <martasagun95@gmail.com>
2023-10-07 19:14:24 +0200

    added a cart element that can be opened and closed by clicking

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,15 +1,19 @@
 import { ReactNode, createContext, useContext, useState } from 'react';
-import { renderToStaticMarkup } from 'react-dom/server';
+import { ShoppingCart } from '../components/ShoppingCart';
 
 type ShoppingCartProviderProps = {
 	children: ReactNode;
 };
 
 type ShoppingCartContext = {
+	openCart: () => void;
+	closeCart: () => void;
 	getItemQuantity: (id: number) => number;
 	increaseCartQuantity: (id: number) => void;
 	decreaseCartQuantity: (id: number) => void;
 	removeFromCart: (id: number) => void;
+	cartQuantity: number;
+	cartItems: CartItem[];
 };
 
 type CartItem = {
@@ -24,12 +28,18 @@ export function useShoppingCart() {
 }
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+	const [isOpen, setIsOpen] = useState(false);
 	const [cartItems, setCartItems] = useState<CartItem[]>([]);
+	const cartQuantity = cartItems.reduce(
+		(quantity, item) => item.quantity + quantity,
+		0
+	);
 
+	const openCart = () => setIsOpen(true);
+	const closeCart = () => setIsOpen(false);
 	function getItemQuantity(id: number) {
 		return cartItems.find((item) => item.id === id)?.quantity || 0;
 	}
-
 	function increaseCartQuantity(id: number) {
 		setCartItems((currItems) => {
 			if (currItems.find((item) => item.id === id) == null) {
@@ -45,7 +55,6 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
 			}
 		});
 	}
-
 	function decreaseCartQuantity(id: number) {
 		setCartItems((currItems) => {
 			if (currItems.find((item) => item.id === id)?.quantity === 1) {
@@ -61,7 +70,6 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
 			}
 		});
 	}
-
 	function removeFromCart(id: number) {
 		setCartItems((currItems) => {
 			return currItems.filter((item) => item.id !== id);
@@ -74,9 +82,14 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
 				increaseCartQuantity,
 				decreaseCartQuantity,
 				removeFromCart,
+				cartItems,
+				cartQuantity,
+				openCart,
+				closeCart,
 			}}
 		>
 			{children}
+			<ShoppingCart isOpen={isOpen} />
 		</ShoppingCartContext.Provider>
 	);
 }
